5aec87a40c8fafbf92cc1bf3b7d488e9962b927b
ash1198 <ash.ranjan09@gmail.com>
2023-11-22 01:23:10 +0530

    added route a2rp

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require("express");
 const app = express();
 
+app.get("/api/v1/a2rp", (req, res) => {
+    res.json({
+        success: true,
+        message: "a2rp: an Ashish Ranjan presentation"
+    });
+});
+
 const PORT = process.env.port || 1198;
 app.listen(PORT, () => {
     console.log(`server running on port ${PORT}: http://localhost:${PORT}`);
